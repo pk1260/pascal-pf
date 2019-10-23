1a5d8847965591cbc53ccc55b9085b9297752684
pk1260 <43113381+pk1260@users.noreply.github.com>
2019-10-23 13:29:20 +0200

    Push-V1.8

--- private/views/project.php
+++ private/views/project.php
@@ -9,22 +9,64 @@
 <div class="project__main">
     <div class="project__header">
         <h2 class="project__header-h2">Portfolio projects</h2>
+        <div class="icon-scroll"></div>
     </div>
 </div>
 
 <div class="project__content-box">
     <div class="project__content">
-        <img src="img/tokyo-2560x1440p.jpg" alt="" class="project__content-img">
-        <a href="" class="project__content-button">LIVE PREVIEW</a>
-        <a href="" class="project__content-button">GITHUB</a>
-
         <h2 class="project__content-h3">Project 1</h2>
-
-        <h4 class="project__content-h4">My role:</h4>
-        <p class="project__content-p">In this project my role was</p>
-        <h4 class="project__content-h4">Project difficulties:</h4>
-        <p class="project__content-p">In this project my role was</p>
-        <h4 class="project__content-h4">My solution:</h4>
-        <p class="project__content-p">In this project my role was</p>
+        <img src="img/shibuya-stuff.png" alt="" class="project__content-img">
+        <div class="project__content-split">
+            <a href="" class="project__content-button" id="button-1">LIVE PREVIEW</a>
+            <a href="" class="project__content-button" id="button-2">GITHUB</a>
+        </div>
+        <div class="project__content-list">
+            <ul class="project__content-list-left">
+                <li>a</li>
+                <li>a</li>
+                <li>a</li>
+            </ul>
+            <ul class="project__content-list-right">
+                <li>a</li>
+                <li>a</li>
+                <li>a</li>
+            </ul>
+        </div>
+        <div class="project__content-group">
+            <h4 class="project__content-h4">My role:</h4>
+            <p class="project__content-p">It is a long established fact that a reader will be distracted by the
+                readable
+                content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a
+                more-or-less normal distribution of letters, as opposed to using 'Content here, content here',
+                making it
+                look like readable English. Many desktop publishing packages and web page editors now use Lorem
+                Ipsum as
+                their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their
+                infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose
+                (injected humour and the like).</p>
+            <h4 class="project__content-h4">Project difficulties:</h4>
+            <p class="project__content-p">It is a long established fact that a reader will be distracted by the
+                readable
+                content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a
+                more-or-less normal distribution of letters, as opposed to using 'Content here, content here',
+                making it
+                look like readable English. Many desktop publishing packages and web page editors now use Lorem
+                Ipsum as
+                their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their
+                infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose
+                (injected humour and the like).</p>
+            <h4 class="project__content-h4">My solution:</h4>
+            <p class="project__content-p">It is a long established fact that a reader will be distracted by the
+                readable
+                content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a
+                more-or-less normal distribution of letters, as opposed to using 'Content here, content here',
+                making it
+                look like readable English. Many desktop publishing packages and web page editors now use Lorem
+                Ipsum as
+                their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their
+                infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose
+                (injected humour and the like).</p>
+        </div>
     </div>
 </div>
